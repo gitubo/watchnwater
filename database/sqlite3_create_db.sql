616a005990a3df80a1dbe611c25261ab0841f3bc
gitubo <mosca.massimiliano@gmail.com>
2014-06-19 17:30:10 +0200

    Update sqlite3_create_db.sql

--- database/sqlite3_create_db.sql
+++ database/sqlite3_create_db.sql
@@ -16,7 +16,7 @@ INSERT INTO [settings] ([name], [int_value]) VALUES ('actuators_number', 4);
 -- Table: sensors_log
 CREATE TABLE sensors_log (
     id                  INTEGER          PRIMARY KEY AUTOINCREMENT,
-    create_date         DATETIME         DEFAULT ( datetime( CURRENT_TIMESTAMP, 'localtime' )  ),
+    [date]              DATETIME         DEFAULT ( datetime( CURRENT_TIMESTAMP, 'localtime' )  ),
     temperature         NUMERIC( 4, 2 ),
     humidity            NUMERIC( 2, 0 ),
     pressure            NUMERIC( 6, 2 ),
@@ -27,18 +27,19 @@ CREATE TABLE sensors_log (
 -- Table: actuators
 CREATE TABLE actuators (
     id                  INTEGER         PRIMARY KEY AUTOINCREMENT,
+    type				INTEGER			NOT NULL,  --0=undef, 1=boolean, 2=integer, 3=string
     description         VARCHAR( 150 )  NOT NULL
 );
 
-INSERT INTO [actuators] ([id], [description]) VALUES (1, 'Relay_1');
-INSERT INTO [actuators] ([id], [description]) VALUES (2, 'Relay_2');
-INSERT INTO [actuators] ([id], [description]) VALUES (3, 'Relay_3');
-INSERT INTO [actuators] ([id], [description]) VALUES (4, 'Relay_4');
+INSERT INTO [actuators] ([id], [type], [description]) VALUES (1, 1, 'Relay_1');
+INSERT INTO [actuators] ([id], [type], [description]) VALUES (2, 1, 'Relay_2');
+INSERT INTO [actuators] ([id], [type], [description]) VALUES (3, 1, 'Relay_3');
+INSERT INTO [actuators] ([id], [type], [description]) VALUES (4, 1, 'Relay_4');
 
 -- Table: actuators_log
 CREATE TABLE actuators_log (
     id                  INTEGER          PRIMARY KEY AUTOINCREMENT,
-    create_date         DATETIME         DEFAULT ( datetime( CURRENT_TIMESTAMP, 'localtime' )  ),
+    [date]	            DATETIME         DEFAULT ( datetime( CURRENT_TIMESTAMP, 'localtime' )  ),
     actuator            INTEGER          REFERENCES actuators ( id ) NOT NULL,
     boolean_value       BOOLEAN,
     int_value           INTEGER,
@@ -48,15 +49,6 @@ CREATE TABLE actuators_log (
 -- Index: idx_actuators_log
 CREATE INDEX idx_actuators_log ON actuators_log (
     actuator
-);
-
--- Table: actuators_status
-CREATE TABLE actuators_status (
-    actuator            INTEGER          REFERENCES actuators ( id ) NOT NULL UNIQUE,
-    date                DATETIME         DEFAULT ( datetime( CURRENT_TIMESTAMP, 'localtime' )  ),
-    boolean_value       BOOLEAN,
-    int_value           INTEGER,
-    string_value        VARCHAR( 64 )
 );
 
 -- Table: actions
@@ -73,18 +65,18 @@ INSERT INTO [actions] ([id], [description]) VALUES (4, 'Turned OFF [forced]');
 -- Table: actions_log
 CREATE TABLE actions_log (
     id                  INTEGER          PRIMARY KEY AUTOINCREMENT,
-    create_date         DATETIME         DEFAULT ( datetime( CURRENT_TIMESTAMP, 'localtime' )  ),
-    type                INTEGER          REFERENCES actions ( id ) NOT NULL,
-    note                VARCHAR( 128 ),
-    from_date           DATETIME,
-    to_date             DATETIME
+    [date]              DATETIME         DEFAULT ( datetime( CURRENT_TIMESTAMP, 'localtime' )  ),
+    action              INTEGER          REFERENCES actions ( id ) NOT NULL,
+    [from]              DATETIME,
+    to                  DATETIME,
+    note                VARCHAR( 128 )
 );
 
 -- Table: watering_plan
 CREATE TABLE watering_plan (
     id                  INTEGER          PRIMARY KEY AUTOINCREMENT,
     actuator            INTEGER          REFERENCES actuators ( id ) NOT NULL,
-    [from]                DATETIME         NOT NULL,
+    [from]              DATETIME         NOT NULL,
     duration            INTEGER          NOT NULL,
     weekdays_bitmask    VARCHAR( 8 )     DEFAULT ('01111111') NOT NULL,
     is_valid            BOOLEAN          DEFAULT ( 1 ) NOT NULL,
